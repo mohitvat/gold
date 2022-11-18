462eb1a113db18cb9fc8adf6bde01079337866e1
mohitvat <mohitvatyai@gmail.com>
2022-11-18 14:35:58 +0530

    -a

---
+++ pages/aboutus.js
@@ -0,0 +1,199 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const aboutus = () => {
+  return (
+    <div>
+      <div className="bg-[#2F2F2F] flex w-full h-screen overflow-hidden text-left text-[55px] text-orange-100 font-montserrat">
+        <div className=" w-[70%] flex flex-col justify-center">
+          <div className="pl-28">
+            <b className="inline-block w-[618px] pb-6">
+              <p className="text-[#FFC565]">Encash Your Old and Scrap Gold</p>
+            </b>
+            <button className="cursor-pointer p-[14px_51px] bg-[#FFC565] rounded-[8px] shadow-[0px_13px_19px_rgba(211,_129,_39,_0.15)] flex flex-row box-border items-start justify-start">
+              <div className=" text-sm tracking-[0.2px] leading-[24px] font-semibold font-montserrat text-black text-center inline-block">
+                CALL US
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div className="relative w-[30%]">
+          <div className=" top-0 right-0 bg-[#FFC565] w-full h-screen" />
+
+          <div className="absolute top-[25%] -left-[160%]">
+            <img
+              className="w-[1200px] h-[548px] object-cover"
+              alt=""
+              src="..\User_Guide_All_You_Need_to_Know_about_Getting_a_Gold_Loan_in_India_a8432c2b7e-removebg-preview 1.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className=" text-2xl font-bold font-montserrat text-[#FFC565] mt-6">
+        <p className="ml-[700px]  ">ABOUT US</p>
+      </div>
+      <div className=" ml-40 box h-[300px] w-[1200px] box-border border-4  border-[#FFC565] rounded-3xl mt-10">
+        <p className="text-center px-8 py-9 font-medium  ">
+          The Name Golden Bird (Sone Ki Chidiya ) proves itself that India’s
+          Fascination For Gold. Back to the ancient times, owing to the Metal’s
+          Religious, Cultural & Investment Significance, Gold continues to
+          capture the imagination of every Indian till time with the highest
+          deposits in the World & there is an insatiable demand for the Gold
+          irrespective of Volatility in its Prices. Instant Liquidity &
+          Investment Benefits are the factors that drive People to Buy & Store
+          Gold. For every Indian, Investments in Gold have always been viewed as
+          a Reliable Source in Times of Need, in Every Rural, Semi-Rural & even
+          Now in Semi-Urban Households. Hence Gold has the Quickest Liquidity to
+          meet any Exigencies or Emergencies of Financial Needs. Gold Loan
+          Companies Offers Minimal and Inadequate Value of the Gold, On top of
+          it the Customer Buys the Interest Burdens & Tensions of failing in
+          Monthly Instalments. GoldCash Limited provides a Quick, Smart,
+          Professional & Hassle Free, Solutions to those Customers to get their
+          Mortgaged Gold freed from any of the Banks, Financial Institutions,
+          NBFC’s and Offers Honest Prices to the Customers and Authentic
+          Evaluation of their Gold. GoldCash Limited has become India’s One of
+          the Well Accepted and Trusted Gold Buyer with Presence across PAN
+          India Geographies.
+        </p>
+      </div>
+      <div className="absolute top-[1150px] bg-[#FFEEDB] w-[1518px] h-[700px] overflow-hidden text-left text-10xs text-black  mt-16">
+        <b className="absolute mt-[150px] ml-[120px] text-6xl font-poppins w-[500px]">
+          <p className="mb-1 ml-1">begin your </p>
+          <p className="gap-1">Golden journey With GoldCash</p>
+        </b>
+        <img
+          className="absolute h-[6.65%] w-[18.19%] top-[51.25%] mr-[74%] mb-[42.1%] ml-[8.00%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="../ctabuttons.svg"
+        />
+        <img
+          className=" h-[287.11%] w-[141.59%] top-[0%] right-[-41.59%] bottom-[-187.11%] left-[0%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="../backgroundelements.svg"
+        />
+        <img
+          className="absolute top-[0px]  w-[1000px] h-[750px] ml-[600px]  "
+          alt=""
+          src="../iphone11progoldmockupremovebgpreview-1@2x.png"
+        />
+        <div className="absolute top-[280px] left-[800px] rounded-[15.1px] bg-white shadow-[0px_9.34896469116211px_13.66px_rgba(255,_197,_101,_0.07)] w-[150px] h-[30px] overflow-hidden ">
+          <b className="absolute px-4 py-1">Sell Your Gold</b>
+        </div>
+
+        <div className="absolute top-[500px] left-[1250px] rounded-[15.1px] bg-white shadow-[0px_9.34896469116211px_13.66px_rgba(255,_197,_101,_0.07)] w-[150px] h-[30px] overflow-hidden ">
+          <b className="absolute px-4 py-1">Buy Your Gold</b>
+        </div>
+        <img
+          className="absolute top-[220px] left-[1300px] w-[80px] h-[100px]"
+          alt=""
+          src="../group-10000019701.svg"
+        />
+        <img
+          className="absolute top-[500px] left-[835px] w-[80px] h-[100px]"
+          alt=""
+          src="../group-1000001971.svg"
+        />
+      </div>
+      <section className="absolute top-[2000px] w-[1200px] h-[700px] ">
+        <div className="  text-3xl font-bold font-montserrat text-[#FFC565]">
+          <p className="ml-[600px]  ">OUR FOUNDER & CEO</p>
+        </div>
+
+        <div className="mt-9 rounded-[12px] bg-[#222222] w-[1100px] h-[500px] ml-48 overflow-hidden"></div>
+
+        <img
+          className="absolute top-[120px] left-[250px]  w-[300px] h-[400px] object-cover"
+          alt=""
+          src="../Group 1000002033.png"
+        />
+        {/* <img
+          className="absolute top-[125px] left-[650px] w-[60px] h-[60px] overflow-hidden"
+          alt=""
+          src="../quote-1.svg"
+        /> */}
+        {/* <img
+          className="absolute top-[280px] left-[1250px] w-[60px] h-[60px] overflow-hidden"
+          alt=""
+          src="../quote-2.svg"
+        /> */}
+        {/* <img
+          className="absolute top-[425px] left-[1260px] w-[58.44px] h-[15.03px]"
+          alt=""
+          src="../group-1000001982.svg"
+        /> */}
+        <div className="absolute top-[170px] left-[600px] text-base  capitalize font-light  font-manrope text-white ">
+          Am saaaaatisfied with your service Acomplination is so clear.. Thank “
+          GOLDCASH LIMITED (an ISO 9001: 2015 Certified Company) is India’s
+          fastest growing Gold Buying Company with presence across PAN India.
+          The Company has successfully tapped an Industry Opportunity on the
+          Largest Consumer Gold Base (Our India) and its Value Growth Drivers,
+          meeting the Consumer’s Needs in Monetizing Gold for Productive
+          Purposes, Increase Demand for Easier, Faster Smarter Liquidity and to
+          create a Platform for Shifting Consumer’s Preferences from Owning
+          Assets (Gold, be it in any Forms) to Cash Currency. GoldCash Visions
+          to Create a Competitive Advantage for the Brand which will Inspire
+          Trust and Consumer Loyalty thereby a Consumer Delight in meeting their
+          Needs. Our Company aspires to Create the Largest Network Base in
+          India, Increase Brand Awareness, Build Company’s Image and believes to
+          Create the Highest Service Quality for Our Customers and Value Long
+          Term Relationship with Our Business Partners. GoldCash has also
+          successfully adopted Professional Services with Esteemed Banks on Gold
+          Loan Debt Grievances, NPA, Loss Accounts Recoveries and has Mutually
+          benefited the Banks & Loaners
+        </div>
+        <div className="absolute top-[100px] left-[700px]  text-3xl font-bold text-[#FFC565]">
+          FOUNDER & CEO'S MESSAGE
+        </div>
+      </section>
+      <section className="absolute top-[2650px] left-[190px] w-[1200px]  text-center text-lg text-[#FFC565] font-montserrat">
+        <div className="absolute">
+          <div className="w-[50px] flex-row space-x-[145px]  ">
+            <b className="mt-36 text-6xl">13K</b>
+            {/* <b className=" text-xl py-6 text-black">States</b> */}
+
+            <b className=" text-6xl">31+</b>
+            <b className=" text-6xl">43+</b>
+            <b className=" text-6xl">150+</b>
+            <b className=" text-6xl px-5">4</b>
+            {/* <b className=" text-xl py-6 text-black">States</b> */}
+          </div>
+        </div>
+      </section>
+      <section className="absolute top-[2750px] left-[200px] w-[1200px]  text-center text-black font-montserrat">
+        <div className="absolute">
+          <div className="w-[50px] flex-row space-x-48  ">
+            <b className="mt-36 text-xl">States</b>
+            {/* <b className=" text-xl py-6 text-black">States</b> */}
+
+            <b className=" text-xl">Cities</b>
+            <b className=" text-xl">Branches</b>
+            <b className=" text-xl">Customer</b>
+            <b className=" text-xl">countries</b>
+            {/* <b className=" text-xl py-6 text-black">States</b> */}
+          </div>
+        </div>
+      </section>
+      <section className="absolute top-[2900px] bg-[#FFBEBE] w-[1380px] h-[140px] rounded-[9px] py-9  ml-16 text-justify font-montserrat">
+        <img
+          className="absolute top-[0px] ml-5 mt-8 w-[39px] h-[39px] overflow-hidden"
+          alt=""
+          src="../warning-1.svg"
+        />
+        <b className=" text-lg ml-20 mt-1 text-[#D32727] ">Warning</b>
+        <p className="text-sm ml-20 pr-6">
+          Trying to Sell Stolen or Spurious Gold Articles is a Criminal Offence,
+          Sellers must provide Proper Documentation to Transact. We do not
+          entertain Minors under any circumstances to Sell Gold. Individuals
+          between the ages 18 to 21 Years must have Consent of their Family
+          Members to Transact. Verification will be done if necessary. GoldCash
+          Limited Reserves all the Rights to reject any Transaction, if found
+          suspicious and can be reported to the appropriate Agencies.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default aboutus;
